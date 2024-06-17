ee56bfd057ac92e120550a6b3b04e54d112b6caa
prasetion <prasetion@gmail.com>
2024-06-17 23:23:40 +0700

    init project

--- script.js
+++ script.js
@@ -4,61 +4,11 @@ const canvas = document.querySelector("canvas.webgl");
 
 const scene = new THREE.Scene();
 
-const axeshHelper = new THREE.AxesHelper(2);
-scene.add(axeshHelper);
-
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 
-// change position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-
-// set position
-mesh.position.set(0.7, -0.6, 1);
-
-// scale
-mesh.scale.x = 2;
-mesh.scale.y = 0.25;
-mesh.scale.z = 0.5;
-
-// rotation
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
-
-// scene.add(mesh);
-
-// group
-const group = new THREE.Group();
-group.scale.y = 2;
-group.rotation.y = 0.2;
-scene.add(group);
-
-// group : cube 1
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube1.position.x = -1.5;
-group.add(cube1);
-
-// group : cube 2
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube2.position.x = 0;
-group.add(cube2);
-
-// group : cube 3
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-cube3.position.x = 1.5;
-group.add(cube3);
+scene.add(mesh);
 
 const sizes = {
   width: 800,
